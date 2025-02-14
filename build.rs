d33e1ae421bafbd3b623328b1551e81740140993
Yangmoooo <yangmoooo@outlook.com>
2025-02-14 12:53:18 +0800

    build: update Windows rc and add high DPI support

--- build.rs
+++ build.rs
@@ -4,7 +4,8 @@ extern crate winres;
 #[cfg(target_os = "windows")]
 fn main() {
     let mut res = winres::WindowsResource::new();
-    res.set_icon("./assets/icon/ezz.ico");
+    res.set_icon("./assets/icon/ezz.ico")
+        .set_manifest_file("./assets/hdpi.manifest.xml");
     res.compile().unwrap();
 }
 
