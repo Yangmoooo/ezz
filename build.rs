86c4045cad62b2682b998dc8f2d1d920f282d4f3
Yangmoooo <yangmoooo@outlook.com>
2025-08-22 23:04:03 +0800

    build: add some metadata

--- build.rs
+++ build.rs
@@ -3,12 +3,12 @@ extern crate winres;
 
 #[cfg(target_os = "windows")]
 fn main() {
-    if std::env::var("CARGO_FEATURE_NO_ICON").is_err() {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("./assets/icon/ezz.ico")
-            .set_manifest_file("./assets/hdpi.manifest.xml");
-        res.compile().unwrap();
-    }
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("./assets/icon/ezz.ico")
+        .set_manifest_file("./assets/hdpi.manifest.xml")
+        .set("FileDescription", "A very light wrapper around 7-Zip")
+        .set("ProductName", "Easy unZip");
+    res.compile().unwrap();
 }
 
 #[cfg(target_os = "linux")]
